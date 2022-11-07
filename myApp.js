ba3376f3ed3300906691cf43694a954874551af1
Hirok Sengupta <senguptahirok@gmail.com>
2022-11-08 00:01:54 +0530

    20221108 changes

--- myApp.js
+++ myApp.js
@@ -3,7 +3,7 @@ let app = express();
 console.log("Hello World");
 let absolutePath1=__dirname + '/public';
 let absolutePath2=__dirname + '/views/index.html';
-app.use('/',function(req,res){
+app.use('/public',function(){
     express.static(absolutePath1)
 });
 app.get('/',function(req,res){
