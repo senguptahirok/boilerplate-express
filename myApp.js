7c5d05582b7ed314355e278d21408cef3aef3f80
Hirok Sengupta <senguptahirok@gmail.com>
2022-11-07 19:28:03 +0530

    20221107: updated myApp.js

--- myApp.js
+++ myApp.js
@@ -2,13 +2,12 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 let absolutePath=__dirname + '/views/index.html';
-app.use('/',function(req,res){
-    express.static(__dirname + '/public')
-});
 app.get('/',function(req,res){
     res.sendFile(absolutePath)
 });
-
+app.use('/',function(req,res){
+    express.static(__dirname + '/public')
+});
 
 
 
