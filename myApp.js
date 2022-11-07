3f24e162e5dcc5bcf6f469cc8ba3a459a7a6d13d
Hirok Sengupta <senguptahirok@gmail.com>
2022-11-07 19:25:21 +0530

    20221107: added app.get method

--- myApp.js
+++ myApp.js
@@ -2,6 +2,9 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 let absolutePath=__dirname + '/views/index.html';
+app.use('/',function(req,res){
+    express.static(__dirname + '/public')
+});
 app.get('/',function(req,res){
     res.sendFile(absolutePath)
 });
