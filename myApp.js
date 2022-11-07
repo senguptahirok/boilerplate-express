bd1be832466ead4123dfec3a287d32f4bfda97ef
Hirok Sengupta <senguptahirok@gmail.com>
2022-11-07 19:14:42 +0530

    20221107: updated the res.sendFile method

--- myApp.js
+++ myApp.js
@@ -2,8 +2,8 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 let absolutePath=__dirname + '/views/index.html';
-app.get(absolutePath,function(req,res){
-    res.send('Hello Express')
+app.get('/',function(req,res){
+    res.sendFile(absolutePath)
 });
 
 
