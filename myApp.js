5c84be35d7d631db2eed5cd412922268a7fae327
Hirok Sengupta <senguptahirok@gmail.com>
2022-11-08 12:44:57 +0530

    20221108: rollback

--- myApp.js
+++ myApp.js
@@ -1,13 +1,9 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-let absolutePath1=__dirname + '/public';
-let absolutePath2=__dirname + '/views/index.html';
-app.use('/public',function(){
-    express.static(absolutePath1)
-});
+let absolutePath=__dirname + '/views/index.html';
 app.get('/',function(req,res){
-    res.sendFile(absolutePath2)
+    res.sendFile(absolutePath)
 });
 
 
