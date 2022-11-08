c61db8a811ac0322a9693a47ed4d4cedd93864b1
Hirok Sengupta <senguptahirok@gmail.com>
2022-11-08 13:26:36 +0530

    20221108: updates

--- myApp.js
+++ myApp.js
@@ -6,8 +6,8 @@ app.get('/',function(req,res){
     res.sendFile(absolutePath)
 });
 let path02=__dirname + '/public';
-app.use('/public',function(express){
-    express.static(path02)
+app.use('/public',function(){
+    app.static(path02)    
 });
 
 
