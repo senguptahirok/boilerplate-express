4a5793bd5066f14aab534bf4cc49e30b8d4fbb9d
Hirok Sengupta <senguptahirok@gmail.com>
2022-11-08 16:13:35 +0530

    20221108: commented out app.get

--- myApp.js
+++ myApp.js
@@ -4,9 +4,9 @@ console.log("Hello World");
 let path01=__dirname + '/public';
 app.use('/public',express.static(path01));
 let absolutePath=__dirname + '/views/index.html';
-app.get('/',function(req,res){
+/* app.get('/',function(req,res){
     res.sendFile(absolutePath)
-});
+});*/
 app.get('/',function(req,res){
     res.json({"message": "Hello json"})
 });
