3e3eab626e0ee716ab9a16e86a851eebc4901e05
Hirok Sengupta <senguptahirok@gmail.com>
2022-11-08 16:36:51 +0530

    20221108: updated /json

--- myApp.js
+++ myApp.js
@@ -1,16 +1,18 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
+
 let path01=__dirname + '/public';
 app.use('/public',express.static(path01));
+
 let absolutePath=__dirname + '/views/index.html';
 app.get('/',function(req,res){
     res.sendFile(absolutePath)
 });
+
+let obj={"message": "Hello json"};
 app.get("/json",function(req,res){
-    res.json({
-        message: "Hello json"
-    });
+    res.json(obj);
 });
 
 
