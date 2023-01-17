e78b01d93e35d7373b33fb51465c66106821bfe1
Hirok Sengupta <senguptahirok@gmail.com>
2023-01-17 19:20:30 +0530

    20230117 upd #01

--- myApp.js
+++ myApp.js
@@ -2,19 +2,19 @@ let express = require('express');
 let app = express();
 require('dotenv').config();
 console.log("Hello World");
+console.log(process.env.MESSAGE_STYLE);
 
 let path01=__dirname + '/public';
 app.use('/public',express.static(path01));
-
+/*
 let absolutePath=__dirname + '/views/index.html';
 app.get('/',function(req,res){
     res.sendFile(absolutePath)
 });
-
+*/
 let obj={"message": "Hello json"};
 app.get("/json",function(req,res){
-    res.json(obj)
-       .process.env.MESSAGE_STYLE});
+    res.json(obj)});
    
 
 
