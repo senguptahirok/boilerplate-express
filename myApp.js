53d3e6052c4e503f3fabe7a19a9407db2976d09c
Hirok Sengupta <senguptahirok@gmail.com>
2023-01-17 19:25:53 +0530

    20230117 upd#02

--- myApp.js
+++ myApp.js
@@ -6,12 +6,12 @@ console.log(process.env.MESSAGE_STYLE);
 
 let path01=__dirname + '/public';
 app.use('/public',express.static(path01));
-/*
+
 let absolutePath=__dirname + '/views/index.html';
 app.get('/',function(req,res){
     res.sendFile(absolutePath)
 });
-*/
+
 let obj={"message": "Hello json"};
 app.get("/json",function(req,res){
     res.json(obj)});
