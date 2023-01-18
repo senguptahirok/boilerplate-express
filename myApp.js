ad0616c6795498a1c85380e25c2dd79ad751111c
Hirok Sengupta <senguptahirok@gmail.com>
2023-01-18 15:43:39 +0530

    20230118 upd 01

--- myApp.js
+++ myApp.js
@@ -14,8 +14,8 @@ app.get('/',function(req,res){
 
 let obj={"message": "Hello json"};
 app.get("/json",function(req,res){
-    res.json(obj)
-       .toUpperCase()});
+    obj=obj.toUpperCase();
+    res.json(obj)});
    
 
 
