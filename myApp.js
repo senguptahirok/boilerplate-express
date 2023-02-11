1c935037a7f9055fa49ad8f682add276974e3ee6
Hirok Sengupta <senguptahirok@gmail.com>
2023-02-11 23:06:44 +0530

    20230211 updated middleware fn.

--- myApp.js
+++ myApp.js
@@ -5,35 +5,21 @@ console.log("Hello World");
 console.log(process.env.MESSAGE_STYLE);
 
 let path01=__dirname + '/public';
-app.use('/public',express.static(path01));
-
-let absolutePath=__dirname + '/views/index.html';
-app.get('/',function(req,res,next){
-    console.log(req.method+' '+req.path+' -'+req.ip);
-    res.sendFile(absolutePath);
-    next();
+/* app.use('/public',express.static(path01)); */
+app.use(function(req,res,next){
+  console.log(req.method + ' ' + req.path +' -' + req.ip);
+  next();
 });
 
+let absolutePath=__dirname + '/views/index.html';
+app.get('/',function(req,res){
+    res.sendFile(absolutePath);});
 let obj={'message': "Hello json"};
-app.get("/json",function(req,res,next){
+
+app.get("/json",function(req,res){
     const MESSAGE_STYLE = process.env['MESSAGE_STYLE'];
     if (MESSAGE_STYLE === 'uppercase')
       obj['message'] = obj['message'].toUpperCase();
-    
-    console.log(req.method+' '+req.path+' -'+req.ip);
-    res.json(obj);
-    next();});
-
-
-
-
-
-
-
-
-
-
-
-
+    res.json(obj);});
 
  module.exports = app;
