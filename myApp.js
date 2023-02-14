3b02fc5069eaf863294eb975706a751d41a04257
Hirok Sengupta <senguptahirok@gmail.com>
2023-02-14 17:14:41 +0530

    updated friend's name

--- myApp.js
+++ myApp.js
@@ -14,7 +14,7 @@ app.get('/now', function(req,res,next){
   console.log('time = ' + req.time);
   next();}, function(req,res){
     res.json({time: req.time});})
-    
+
 let path01=__dirname + '/public';
 app.use('/public',express.static(path01));
 
@@ -28,5 +28,12 @@ app.get("/json",function(req,res){
     if (MESSAGE_STYLE === 'uppercase')
       obj['message'] = obj['message'].toUpperCase();
     res.json(obj);});
+
+let obj_friend={'message': "Hello Joel"};
+app.get("/friend",function(req,res){
+    const MESSAGE_STYLE = process.env['MESSAGE_STYLE'];
+    if (MESSAGE_STYLE === 'uppercase')
+       obj_friend['message'] = obj_friend['message'].toUpperCase();
+    res.json(obj_friend);});        
 
 module.exports = app;
